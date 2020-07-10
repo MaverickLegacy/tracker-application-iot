21542e976f0005a38ab91a4c26044efabba966b2
kashyap <sreesha.kashyap@mavs.uta.edu>
2020-07-09 20:15:29 -0500

    Adding intial Project Structure.

---
+++ api/src/test/java/io/egen/car_tracker_application/CarTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.egen.car_tracker_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
